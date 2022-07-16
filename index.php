fc210410f08fb10775d93983e2a678be6c6ecc9a
Dinesh K <dineshk.login@gmail.com>
2022-07-16 22:39:47 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+	<style type="text/css">
+		.indus_education_0 {
+			background-image: url(http://localhost/designpluz/assets/images/service-shape.png);
+		    margin-top: -70px;
+		    position: absolute;
+		    margin-left: 60px;
+		    width: 124px;
+		}
+
+		.indus_education {
+			border: 0px solid red;
+			position: absolute;
+			background-color: #0db28b;
+			margin-top:  -60px;
+			margin-left: 60px;
+			width: 140px;
+			padding:  2px  0px;
+			text-align: center;
+			color: #FFFFFF;
+		}
+
+		.idus_cont {
+			position: relative;
+			float:  left;
+			width: 260px;
+		}
+		.recent_project {
+			position: relative;
+		    float: left;
+		    max-width: 266px;
+		    margin-left: -8px;
+		    margin-right: 142px;
+		}
+		.indus_div {
+			margin-top: -4px;
+    		background-color: black;
+   			padding: 10px;
+    		color: white;
+		}
+		 hr {
+	       	position: relative;
+		    top: 189px;
+		    border: none;
+		    height: 1px;
+		    background: grey;
+		    margin-bottom: 50px;
+    	}
+    	/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: right;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add an active class to highlight the current page */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Hide the link that should open and close the topnav on small screens */
+.topnav .icon {
+  display: none;
+}
+.xiteaudio_text{
+    width: 344px;
+    padding:  14px 0px 16px 15px;
+    font-weight: 900;
+    float:  left;
+}
+
+	span p {
+    margin: 0;
+}
+.xiteaudio_textround{
+    border-radius: 50%;
+    border: 2px solid #b5d789;
+    float: left;
+    margin-left: 25%;
+    height: 150px;
+    width:  150px;
+}
+
+.testimonial_about{
+	margin: 10px 0px;
+	border: 1px solid green;
+}
+		
+		.clear { clear: both; }
+
+.about_price{
+	  font-weight: 900;
+    font-size: 40px;
+    padding-left: 35%;
+    padding-top: 16%;
+}
+
+.about_year{
+	  font-weight: 900;
+    padding-left: 35%;
+}
+
+.about_exp { 
+		font-weight: 900;
+    /* font-size: 13px; */
+    padding-left: 15%; 
+  }
+
+	</style>
+</head>
+<body>
+<div style="margin: 0 auto; border: 1px solid red; width: 800px;">
+	
+		
+		<div class="topnav" id="myTopnav">
+				<a href="#home" class=""><span style="color: #0db28b">T:1300 568 571 </span></a>
+				<a href="#contactus" class="">Contact Us</a>
+				<a href="#clients" class="">Clients</a>
+		  	<a href="#projects" class="">Projects</a>
+				<a href="#xcitecare">XciteCare</a>
+				<a href="#industries">Industries</a>
+				<a href="#about">About</a>
+	 </div>
+			<div>
+				<img style="max-width: 800px;" src="assets/images/hero-banner.jpg">
+			</div>
+
+			
+			<div class="testimonial_about">
+				<div class="clear"></div>
+					<div class="xiteaudio_text"><span style="color: #0db28b">Xcite Audio Visual</span> is a technology based company focused on providing reliable, robust solutions that enhance the user experience for our client's projects.</div>
+
+					<div class="xiteaudio_textround">
+						<div class="about_price">50</div>
+						<div class="about_year">YEARS</div>
+						<div class="about_exp">EXPERIENCE</div>
+					</div>
+				<div class="clear"></div>
+			</div>
+
+			
+		
+			<div class="indus_div" >
+				<center>Industries</center>
+			</div>
+		
+
+
+			<div class="idus_cont">
+				<img style="max-width: 266px ;" src="assets/images/service_education.jpg">
+				<span class="indus_education_0">&nbsp;</span>
+				<span class="indus_education">Education</span>
+			</div>
+
+			<div class="idus_cont">
+				<img style="max-width: 266px" src="assets/images/service_corporate.jpg">
+				<span class="indus_education_0">&nbsp;</span>
+				<span class="indus_education">Corporate</span>
+			</div>
+
+			<div class="idus_cont">
+				<img style="max-width: 266px" src="assets/images/service_government.jpg">
+				<span class="indus_education_0">&nbsp;</span>
+				<span class="indus_education">Government</span>
+			</div>
+
+			<div style="clear:both;"></div>
+			<div style="margin-top: 30px;">
+			<center>Our Recent Projects</center>
+			</div>
+			<div>
+				<div class="recent_project">
+					<img style="max-width: 393px;
+			    height: 194px;" src="assets/images/recent_project_01.jpg">
+				</div>
+				<div class="recent_project">
+					<img style="max-width: 393px;
+			    height: 194px;" src="assets/images/recent_project_02.jpg">
+				</div>
+			</div>
+			<div class="idus_cont">
+				<img style="max-width: 266px ;" src="assets/images/recent_project_03.jpg">
+				
+			</div>
+
+			<div class="idus_cont">
+				<img style="max-width: 266px" src="assets/images/recent_project_05.jpg">
+			</div>
+			<div class="idus_cont">
+				<img style="max-width: 265px; height: 189px;" src="assets/images/recent_project_06.jpg">	
+			</div>
+			<hr>
+			<div></div>
+		 	
+ </div>
+ 		
+ 		<div></div>
+	
+
+
+</body>
+</html>
